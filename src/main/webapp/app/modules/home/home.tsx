898909f266e6760f3b02c1cfd55f1db12c4bd3c5
Bruno Caruso <brunofcaruso@gmail.com>
2020-08-15 00:08:13 -0300

    Creado componente Products
    Se creo el componente Products donde se puso el contenido del tabMenu, el componente
    Products se muestra ahora en el componente Home si es que el usuario inicio sesion.

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -4,84 +4,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Alert } from 'reactstrap';
 
-import { createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
-
-import { makeStyles, Theme, ThemeProvider } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+// Importo componente Products al componente Home del sitio
+import Products from '../products/products'
 
 export type IHomeProp = StateProps;
-
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: any;
-  value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`scrollable-force-tabpanel-${index}`}
-      aria-labelledby={`scrollable-force-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: any) {
-  return {
-    id: `scrollable-force-tab-${index}`,
-    'aria-controls': `scrollable-force-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    flexGrow: 1,
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
 
 export const Home = (props: IHomeProp) => {
   const { account } = props;
 
-  const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
   };
 
-  const tema = createMuiTheme({
-    palette: {
-      primary: {
-        main: '#2a699d',
-      },
-    },
-  });
-
   return (
-    <ThemeProvider theme={tema}>
+    
     <div>
       
         <h2>
@@ -89,32 +27,7 @@ export const Home = (props: IHomeProp) => {
         </h2>
         {account && account.login ? (
           <div>
-            <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          variant="scrollable"
-          scrollButtons="on"
-          indicatorColor="primary"
-          textColor="primary"
-          aria-label="scrollable force tabs example"
-        >
-          <Tab label="Encargado" icon={<ShoppingBasket />} {...a11yProps(0)} />
-          <Tab label="Enviado" icon={<ArrowUpwardIcon />} {...a11yProps(1)} />
-          <Tab label="Entregado" icon={<CheckCircleIcon />} {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        Pedidos encargados
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Pedidos enviados
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Pedidos entregados
-      </TabPanel>
-    </div>
+          <Products/>
           </div>
         ) : (
           <div>
@@ -123,12 +36,7 @@ export const Home = (props: IHomeProp) => {
             </Alert>
           </div>
         )}
-
-
-
-      
     </div>
-    </ThemeProvider>
   );
 };
 
